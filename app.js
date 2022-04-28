4792b8f66e218d1c20c81e76354e5efac61a2cc8
ayindesegun <ayindesegun17@gmail.com>
2022-04-28 11:41:57 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,26 +1,111 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const _ = require('lodash')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-let items = ['Buy food', 'Cook food', 'Eat food']
-app.get('/', (req, res) => {
-  var today = new Date()
-  var options = {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-  }
-  var day = today.toLocaleDateString('en-US', options)
 
-  res.render('list', { listTitle: day, newListItems: items })
+mongoose.connect('mongodb+srv://admin-segun:ayinseg@cluster0.gebxu.mongodb.net/todolistDB')
+/* let items = ['Buy food', 'Cook food', 'Eat food'] */
+const itemsSchema = {
+  name: String,
+}
+const Item = mongoose.model('Item', itemsSchema)
+const item1 = new Item({
+  name: 'Welcome to your todo list',
 })
+const item2 = new Item({
+  name: 'Hit the + button to add to the list',
+})
+const item3 = new Item({
+  name: '<--- Hit this to delete an item',
+})
+
+const defaultItems = [item1, item2, item3]
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+}
+const List = mongoose.model('List', listSchema)
+
+app.get('/', (req, res) => {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('Success!')
+        }
+      })
+      res.redirect('/')
+    } else {
+      res.render('list', { listTitle: 'Today', newListItems: foundItems })
+    }
+  })
+})
+
+app.get('/:customRoute', (req, res) => {
+  const customRoute = _.capitalize(req.params.customRoute)
+  List.findOne({ name: customRoute }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        //create new list
+        const list = new List({
+          name: customRoute,
+          items: defaultItems,
+        })
+
+        list.save()
+        res.redirect('/' + customRoute)
+      } else {
+        // Show an existing list
+        res.render('list', {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        })
+      }
+    }
+  })
+})
+
 app.post('/', (req, res) => {
-  let item = req.body.newItem
-  items.push(item)
-  res.redirect('/')
+  let itemName = req.body.newItem
+  const listName = req.body.list
+  const item = new Item({
+    name: itemName,
+  })
+  if (listName === 'Today') {
+    item.save()
+    res.redirect('/')
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      foundList.items.push(item)
+      foundList.save()
+      res.redirect('/' + listName)
+    })
+  }
+})
+app.post('/delete', (req, res) => {
+  const checkedItemId = req.body.checkbox
+  const listName = req.body.listName
+  if (listName === 'Today') {
+    Item.findByIdAndRemove(checkedItemId, (err) => {
+      if (!err) {
+        console.log('Successfully deleted item')
+        res.redirect('/')
+      }
+    })
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull : {items: {_id: checkedItemId }}}, (err, foundList) => {
+      if (!err){
+        res.redirect('/' + listName)
+      }
+    })
+  }
 })
 app.listen(3000, () => {
   console.log('App is listening on port 3000!')
